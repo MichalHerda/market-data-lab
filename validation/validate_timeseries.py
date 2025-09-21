46b1ac339dadcded585fae062d97f61cbe5389f4
Michal Herda <michalherda83@gmail.com>
2025-09-21 17:34:42 +0200

    add TIMERFRAM_TO_DELTA distionary and is_timeseries_continuous function

--- validation/validate_timeseries.py
+++ validation/validate_timeseries.py
@@ -1,6 +1,38 @@
 import pandas as pd
 import os
 
+TIMEFRAME_TO_DELTA = {
+    "M1": pd.Timedelta(minutes=1),
+    "M5": pd.Timedelta(minutes=5),
+    "M15": pd.Timedelta(minutes=15),
+    "M30": pd.Timedelta(minutes=30),
+    "H1": pd.Timedelta(hours=1),
+    "H4": pd.Timedelta(hours=4),
+    "D1": pd.Timedelta(days=1),
+    "W1": pd.DateOffset(weeks=1),
+    "MN1": pd.DateOffset(months=1)
+}
+
+
+def is_timeseries_continuous(df: pd.DataFrame, timeframe: str) -> bool:
+    if timeframe not in TIMEFRAME_TO_DELTA:
+        raise ValueError(f"Unsupported timeframe: {timeframe}")
+
+    expected_delta = TIMEFRAME_TO_DELTA[timeframe]
+
+    diffs = df["timestamp"].diff().dropna()
+
+    # timedelta (np. dla minut, godzin, dni)
+    if isinstance(expected_delta, pd.Timedelta):
+        return diffs.eq(expected_delta).all()
+
+    # dateoffset (np. tygodnie, miesiące)
+    if isinstance(expected_delta, pd.DateOffset):
+        shifted = df["timestamp"].shift(1) + expected_delta
+        return (df["timestamp"].dropna() == shifted.dropna()).all()
+
+    return False
+
 
 def get_valid_file(prompt: str) -> str:
     """Gets the path to an existing file from the user"""
@@ -14,4 +46,12 @@ def get_valid_file(prompt: str) -> str:
 
 if __name__ == "__main__":
     file = get_valid_file("Enter the file location: ")
-    df = pd.read_csv(file)
+    df = pd.read_csv(file, sep=";", parse_dates=["timestamp"])
+    csv_lenght = 0
+
+    for i in df.index:
+        csv_lenght += 1
+
+    print("csv file lenght: ", csv_lenght)
+    print("data types: \n", df.dtypes)
+    print("head: ", df.head())
